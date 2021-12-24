b4fd0fc41b9d4077e6012dbd70ca675d8c58b911
Xiao <49285022+xiao-title@users.noreply.github.com>
2021-12-25 03:23:41 +0900

    modify

--- src/views/navbar/index.style.tsx
+++ src/views/navbar/index.style.tsx
@@ -7,21 +7,88 @@ export const NavbarWrapper = styled.div`
   border-radius: 10px;
   box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
   grid-gap: 40px;
-  a{
+  display: flex;
+  justify-content: space-between;
+  a {
     text-decoration: none;
   }
   position: fixed;
   width: calc(100vw - 250px);
   z-index: 1;
-  @media (max-width: 992px) {
-    display:none;
-  } 
-`
 
+  @media (max-width: 992px) {
+    display: none;
+  }
+
+  svg {
+    vertical-align: middle;
+  }
+
+  #avatar-tip {
+    background-color: #e8b89b;
+    color: #fff;
+    border: 1px solid #e8b89b;
+    font-weight: bold;
+    font-size: 14px;
+
+    &:after {
+      border-top-color: #e8b89b;
+    }
+  }
+
+  .menu {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    margin-top: 60px;
+
+    box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175);
+
+    .item {
+      color: #fff;
+      background-color: #e8b89b;
+      padding: 10px;
+
+      cursor: pointer;
+
+      :hover {
+        background-color: #c5b097;
+      }
+    }
+  }
+`;
 
 export const MenuLabel = styled.span`
   color: #ecc5ac;
   font-weight: 600;
   font-size: 1.1em;
   text-transform: uppercase;
-`
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+
+export const UserButton = styled.button`
+  cursor: pointer;
+  color: #e8b89b;
+  font-weight: 600;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 0;
+`;
+
+export const ProfileImage = styled.img`
+  width: 35px;
+  height: 35px;
+
+  border-radius: 50%;
+  border: 3px solid;
+`;
+
+export const UserAction = styled.div`
+  display: flex;
+  justify-content: right;
+`;
--- src/views/navbar/index.tsx
+++ src/views/navbar/index.tsx
@@ -1,16 +1,112 @@
-import React from "react";
-import {HashLink as Link} from "react-router-hash-link";
-import {NavbarWrapper, MenuLabel} from "./index.style";
-
+import jwtDecode from "jwt-decode";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
+import { toast } from "react-toastify";
+import useAuth from "../../hooks/useAuth";
+import {
+  NavbarWrapper,
+  MenuLabel,
+  LinkContainer,
+  UserButton,
+  ProfileImage,
+  UserAction,
+} from "./index.style";
 
 export default function Navbar() {
+  const [profileImg, setProfileImg] = useState<string>("");
 
-    return (
-            <NavbarWrapper>
-                <Link to="/"><MenuLabel>home</MenuLabel></Link>
-                <Link to="/#about"><MenuLabel>sobre nós</MenuLabel></Link>
-                <Link to="/#howto"><MenuLabel>como funciona?</MenuLabel></Link>
-                <Link to="/blog"><MenuLabel>blog</MenuLabel></Link>
-            </NavbarWrapper>
-    );
+  const { isAuthenticated, logout, user } = useAuth();
+
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+  const handleLogout = async (): Promise<void> => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+      toast.error("Unable to logout");
+    }
+  };
+
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleDashboard = async (): Promise<void> => {
+    setShowMenu(false);
+    const accessToken: any = window.localStorage.getItem("accessToken");
+    const token: any = jwtDecode(accessToken);
+    if (token.role == 0) {
+      navigate("/vendor-dashboard");
+    } else {
+      navigate("/painel-usuario");
+    }
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    try {
+      const accessToken: any = window.localStorage.getItem("accessToken");
+      const decoded: any = jwtDecode(accessToken);
+      if (decoded.avataUrl == "" || typeof decoded.avataUrl == "undefined") {
+        setProfileImg("/user-profile.png");
+      } else {
+        setProfileImg(decoded.avataUrl);
+      }
+    } catch (err) {
+      setProfileImg("/user-profile.png");
+    }
+
+    const checkIfClickedOutside = (e: any) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, []);
+  return (
+    <NavbarWrapper>
+      <LinkContainer>
+        <Link to="/">
+          <MenuLabel>home</MenuLabel>
+        </Link>
+        <Link to="/#about">
+          <MenuLabel>sobre nós</MenuLabel>
+        </Link>
+        <Link to="/#howto">
+          <MenuLabel>como funciona?</MenuLabel>
+        </Link>
+        <Link to="/blog">
+          <MenuLabel>blog</MenuLabel>
+        </Link>
+      </LinkContainer>
+      <UserAction>
+        <UserButton onClick={handleShowMenu} data-tip data-for="avatar-tip">
+          <ProfileImage src={profileImg} />
+        </UserButton>
+        {showMenu ? (
+          <div className="menu" ref={inputRef}>
+            <div className="item" onClick={handleDashboard}>
+              Painel
+            </div>
+            <div className="item" onClick={handleLogout}>
+              Sair
+            </div>
+          </div>
+        ) : null}
+      </UserAction>
+    </NavbarWrapper>
+  );
 }
